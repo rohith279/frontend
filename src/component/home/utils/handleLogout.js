48af9ce38189b621ff905d0c8a83591f78547cdc
rohith279 <vuyyalar@coventry.ac.uk>
2024-08-11 17:55:15 +0000

    Payment Pending

--- src/component/home/utils/handleLogout.js
+++
@@ -1,9 +0,0 @@
-const handleLogout = async ({ doSignOut, navigation }) => {
-  await doSignOut().then(() => {
-    if (window.location.pathname !== "/") {
-      navigation("/");
-    }
-  });
-};
-
-export default handleLogout;
--- src/consts.js
+++
@@ -1,5 +0,0 @@
-export const USERS_COLLECTION = "users";
-export const CONVERSATIONS_COLLECTION = "conversations";
-export const USER_ROLE = "user";
-export const BOT_ROLE = "bot";
-export const SYSTEM_ROLE = "system";
